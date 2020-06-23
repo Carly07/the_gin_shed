4681693de33328c0346af0f7e848b26846f6c5d6
Carly07 <carlysimpson07@hotmail.co.uk>
2020-06-23 21:35:45 +0000

    added urls for blog

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.get_posts, name='get_posts'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('add/', views.create_or_edit_post, name='add_post'),
+    path('edit/<int:pk>/', views.create_or_edit_post, name='edit_post'),
+    path('delete/<int:pk>/', views.delete_post, name='delete_post'),
+]
---
+++ blog/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.utils import timezone
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import BlogPostForm
+
+
+def get_posts(request):
+    """
+    Create a view that will return a list
+    of Posts that were published prior to 'now'
+    and render them to the 'blogposts.html' template
+    """
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+            ).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
+
+
+def post_detail(request, pk):
+    """
+    Create a view that returns a single
+    post object based on the post ID (pk) and
+    render it to the postdetail.html template.
+    Or return a 404 error if the post is
+    not found.
+    """
+
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+    return render(request, "postdetail.html", {'post': post})
+
+
+@login_required
+def create_or_edit_post(request, pk=None):
+    """
+    Create a view that allows us to create
+    or edit a post depending if the post ID
+    is null or not
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry you do are not authorised to perform this action')
+        return redirect(reverse('home'))
+
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            messages.success(request, 'Success! Your Blog has been posted!')
+            return redirect(post_detail, post.pk)
+        else:
+            messages.error(request, 'Oops! Failed to post blog. Please ensure the form is valid.')
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blogpostform.html', {'form': form})
+
+
+@login_required
+def delete_post(request, pk):
+    """ Delete a blog post """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry you do are not authorised to perform this action')
+        return redirect(reverse('home'))
+
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    messages.success(request, 'Post deleted!')
+    return redirect(reverse('posts'))
