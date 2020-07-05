8335396af6aba7fd298d9ca88ee96de7a8af9388
Carly07 <carlysimpson07@hotmail.co.uk>
2020-07-05 15:36:48 +0000

    added views, urls and templates for about, privacy and terms and conditions pages

--- home/views.py
+++ home/views.py
@@ -7,3 +7,21 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about page """
+
+    return render(request, 'home/about.html')
+
+
+def privacy(request):
+    """ A view to return the privacy page """
+
+    return render(request, 'home/privacy.html')
+
+
+def terms(request):
+    """ A view to return the terms and conditions page """
+
+    return render(request, 'home/terms.html')
