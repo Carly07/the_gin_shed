f7b566b3b1288c7f58d8a36bb86c79d729ccc395
Carly07 <carlysimpson07@hotmail.co.uk>
2020-07-11 13:34:24 +0000

    added login details and links to update email and password to profile page

--- gin_shed/forms.py
+++ gin_shed/forms.py
@@ -1,6 +1,6 @@
 from allauth.account.forms import SignupForm 
 from django import forms
-  
+
 class CustomSignupForm(SignupForm): 
     first_name = forms.CharField(max_length=30) 
     last_name = forms.CharField(max_length=30) 
