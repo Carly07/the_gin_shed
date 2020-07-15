4b6960a714e5833f53de6eff2eb2bb36ba860285
Carly07 <carlysimpson07@hotmail.co.uk>
2020-07-15 20:58:21 +0000

    removed secret_key and set debug

--- gin_shed/settings.py
+++ gin_shed/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w1_1w@p16bc)om$h6ql-%tws51kdm+q!7@0&#q#d)m!)^w5q=z'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-gin-shed.herokuapp.com', 'localhost']
 
