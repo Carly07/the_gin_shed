096c130274cdeefea909d476d556913e06d21f0f
Carly07 <carlysimpson07@hotmail.co.uk>
2020-07-18 16:18:54 +0000

    edited view for contact form

--- home/views.py
+++ home/views.py
@@ -34,13 +34,18 @@ def contact(request, *args, **kwargs):
             message = form.cleaned_data.get['message']
 
             try:
-                send_mail(name, message, ['carlyclark07@gmail.com'], email)
+                send_mail(
+                    f'Message for The Gin Shed'
+                    f'New message from ['name'], ['email']:
+                    \n{data['message']}",
+                    settings.DEFAULT_FROM_EMAIL,
+                    ["carlyclark07@gmail.com"])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
 
             messages.success(request, "Thank you for your message. We'll be in touch shortly.")
             return redirect('home')
-            
+
     return render(request, "home/contact.html", {'form': form})
 
 
