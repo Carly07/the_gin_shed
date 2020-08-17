a73e535e2922b46be4d8820c97e85b523f937ec2
Carly07 <carlysimpson07@hotmail.co.uk>
2020-08-17 19:19:28 +0000

    adjust history back function

--- static/js/main.js
+++ static/js/main.js
@@ -7,9 +7,3 @@ $(document).ready(function () {
     $('#copyright-year').text(new Date().getFullYear());
 });
 
-// history back function for Keep shopping button product detail page 
-// so user can return to their previous search results
-
-function goBack() {
-  window.history.go(-1);
-}
