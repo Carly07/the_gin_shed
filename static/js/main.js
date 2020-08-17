06c485df9761e447e752dc73a65f8901653d5975
Carly07 <carlysimpson07@hotmail.co.uk>
2020-08-17 19:11:33 +0000

    added hitory back function to main.js

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,10 @@ $(document).ready(function () {
 
     $('#copyright-year').text(new Date().getFullYear());
 });
+
+// history back function for Keep shopping button product detail page 
+// so user can return to their previous search results
+
+function goBack() {
+  window.history.go(-);
+}
