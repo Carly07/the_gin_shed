9b4eecab6beee9901998b4413abceb0d30e15684
Carly07 <carlysimpson07@hotmail.co.uk>
2020-08-18 19:10:57 +0000

    added timeout function to messages container and moved map javascript to own file

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,11 @@
+
+// Set copyright date to current year
 $(document).ready(function () {
-
-    /**
-     * Set copyright date to current year 
-     */
-
     $('#copyright-year').text(new Date().getFullYear());
 });
 
 
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 53.1897947, lng: -2.896595};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 9, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
+// Set timeout on messages container
+setTimeout(function () {
+    $('#msg-container').fadeOut('slow');
+}, 4000);
